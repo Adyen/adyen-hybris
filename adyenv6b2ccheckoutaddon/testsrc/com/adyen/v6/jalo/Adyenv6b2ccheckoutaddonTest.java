d92ddac8f63b32f34c80f5dac58d66a7311f7d29
Giorgos Adam <georgios.adam@adyen.com>
2016-12-06 13:14:50 +0100

    Initial commitadyenv6b2ccheckoutaddon2

---
+++ adyenv6b2ccheckoutaddon/testsrc/com/adyen/v6/jalo/Adyenv6b2ccheckoutaddonTest.java
@@ -0,0 +1,54 @@
+/*
+ * [y] hybris Platform
+ *
+ * Copyright (c) 2000-2016 SAP SE or an SAP affiliate company.
+ * All rights reserved.
+ *
+ * This software is the confidential and proprietary information of SAP
+ * ("Confidential Information"). You shall not disclose such Confidential
+ * Information and shall use it only in accordance with the terms of the
+ * license agreement you entered into with SAP.
+ */
+package com.adyen.v6.jalo;
+
+import static org.junit.Assert.assertTrue;
+
+import de.hybris.platform.testframework.HybrisJUnit4TransactionalTest;
+
+import org.apache.log4j.Logger;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+
+/**
+ * JUnit Tests for the Adyenv6b2ccheckoutaddon extension
+ */
+public class Adyenv6b2ccheckoutaddonTest extends HybrisJUnit4TransactionalTest
+{
+	/** Edit the local|project.properties to change logging behaviour (properties log4j.*). */
+	@SuppressWarnings("unused")
+	private static final Logger LOG = Logger.getLogger(Adyenv6b2ccheckoutaddonTest.class.getName());
+
+	@Before
+	public void setUp()
+	{
+		// implement here code executed before each test
+	}
+
+	@After
+	public void tearDown()
+	{
+		// implement here code executed after each test
+	}
+
+	/**
+	 * This is a sample test method.
+	 */
+	@Test
+	public void testAdyenv6b2ccheckoutaddon()
+	{
+		final boolean testTrue = true;
+		assertTrue("true is not true", testTrue);
+	}
+}
