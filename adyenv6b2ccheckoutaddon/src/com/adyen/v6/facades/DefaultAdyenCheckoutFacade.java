9cd60b792b83d17f3ad816f1cd98be39053ea33a
rajni <rajni.kewlani@adyen.com>
2018-09-19 10:41:33 +0200

    PW-542: corrected if condition.

--- adyenv6b2ccheckoutaddon/src/com/adyen/v6/facades/DefaultAdyenCheckoutFacade.java
+++ adyenv6b2ccheckoutaddon/src/com/adyen/v6/facades/DefaultAdyenCheckoutFacade.java
@@ -278,7 +278,7 @@ public class DefaultAdyenCheckoutFacade implements AdyenCheckoutFacade {
 
         updateCartWithSessionData(cartData);
 
-        if(PAYMENT_METHOD_CC.equals(cartData.getAdyenPaymentMethod())|| PAYMENT_METHOD_ONECLICK.indexOf(cartData.getAdyenPaymentMethod()) == 0) {
+        if(PAYMENT_METHOD_CC.equals(cartData.getAdyenPaymentMethod())|| cartData.getAdyenPaymentMethod().indexOf(PAYMENT_METHOD_ONECLICK) == 0) {
             PaymentsResponse paymentsResponse = getAdyenPaymentService().authorisePayment(cartData, request, customer);
             if(PaymentsResponse.ResultCodeEnum.AUTHORISED == paymentsResponse.getResultCode()) {
                 return createAuthorizedOrder(paymentsResponse);
