2568f3b4641c68c8042f42f37b1ad01a40da6aa8
joao <joao.santos@adyen.com>
2018-09-28 16:32:26 -0300

    PW-621 accept boletos via OCC

--- adyenv6b2ccheckoutaddon/src/com/adyen/v6/facades/DefaultAdyenCheckoutFacade.java
+++ adyenv6b2ccheckoutaddon/src/com/adyen/v6/facades/DefaultAdyenCheckoutFacade.java
@@ -481,6 +481,11 @@ public class DefaultAdyenCheckoutFacade implements AdyenCheckoutFacade {
         OrderModel orderModel = orderRepository.getOrderModel(orderData.getCode());
         updateOrder(orderModel, paymentsResponse);
 
+        orderData.setAdyenBoletoUrl(paymentsResponse.getBoletoUrl());
+        orderData.setAdyenBoletoData(paymentsResponse.getBoletoData());
+        orderData.setAdyenBoletoExpirationDate(paymentsResponse.getBoletoExpirationDate());
+        orderData.setAdyenBoletoDueDate(paymentsResponse.getBoletoDueDate());
+
         return orderData;
     }
 
@@ -679,6 +684,8 @@ public class DefaultAdyenCheckoutFacade implements AdyenCheckoutFacade {
         paymentInfo.setAdyenPaymentMethod(paymentDetails.getAdyenPaymentMethod());
         paymentInfo.setAdyenSelectedReference(paymentDetails.getAdyenSelectedReference());
         paymentInfo.setAdyenSocialSecurityNumber(paymentDetails.getAdyenSocialSecurityNumber());
+        paymentInfo.setAdyenFirstName(paymentDetails.getAdyenFirstName());
+        paymentInfo.setAdyenLastName(paymentDetails.getAdyenLastName());
 
         modelService.save(paymentInfo);
 
