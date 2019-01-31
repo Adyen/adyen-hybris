fd8522edc46272064fe0e602cda57d6ae887a274
rajni <rajni.kewlani@adyen.com>
2019-01-31 11:48:00 +0100

    [PW-936]: Moved populators from checkoutaddon to core module.

--- adyenv6core/src/com/adyen/v6/populator/AbstractOrderPopulator.java
+++ adyenv6core/src/com/adyen/v6/populator/AbstractOrderPopulator.java
@@ -20,6 +20,7 @@
  */
 package com.adyen.v6.populator;
 
+import org.springframework.beans.factory.annotation.Required;
 import de.hybris.platform.commercefacades.order.data.AbstractOrderData;
 import de.hybris.platform.commercefacades.order.data.CCPaymentInfoData;
 import de.hybris.platform.commercefacades.user.data.AddressData;
@@ -30,7 +31,6 @@ import de.hybris.platform.core.model.order.payment.PaymentInfoModel;
 import de.hybris.platform.core.model.user.AddressModel;
 import de.hybris.platform.servicelayer.dto.converter.ConversionException;
 import de.hybris.platform.servicelayer.dto.converter.Converter;
-import org.springframework.beans.factory.annotation.Required;
 
 public class AbstractOrderPopulator implements Populator<AbstractOrderModel, AbstractOrderData> {
     private Converter<AddressModel, AddressData> addressConverter;
