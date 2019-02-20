834019072be0218d6b68c1908e2d45fa0b03223a
rajni <rajni.kewlani@adyen.com>
2019-02-20 14:25:15 +0100

    [PW-985]: OCC Boleto issue Fix

--- adyenv6core/src/com/adyen/v6/facades/DefaultAdyenCheckoutFacade.java
+++ adyenv6core/src/com/adyen/v6/facades/DefaultAdyenCheckoutFacade.java
@@ -308,6 +308,10 @@ public class DefaultAdyenCheckoutFacade implements AdyenCheckoutFacade {
             return createOrderFromPaymentsResponse(paymentsResponse);
         }
 
+        if (PaymentsResponse.ResultCodeEnum.PRESENTTOSHOPPER == paymentsResponse.getResultCode()) {
+            return createOrderFromPaymentsResponse(paymentsResponse);
+        }
+
         throw new AdyenNonAuthorizedPaymentException(paymentsResponse);
     }
 
@@ -401,7 +405,7 @@ public class DefaultAdyenCheckoutFacade implements AdyenCheckoutFacade {
         updateCartWithSessionData(cartData);
         String adyenPaymentMethod = cartData.getAdyenPaymentMethod();
 
-        if (adyenPaymentMethod.equals(PAYMENT_METHOD_BOLETO) || adyenPaymentMethod.equals(PAYPAL_ECS) || adyenPaymentMethod.startsWith(RATEPAY)) {
+        if (adyenPaymentMethod.equals(PAYPAL_ECS) || adyenPaymentMethod.startsWith(RATEPAY)) {
 
             PaymentResult paymentResult = getAdyenPaymentService().authorise(cartData, request, customer);
             if (PaymentResult.ResultCodeEnum.AUTHORISED == paymentResult.getResultCode()) {
@@ -418,6 +422,9 @@ public class DefaultAdyenCheckoutFacade implements AdyenCheckoutFacade {
             return createAuthorizedOrder(paymentsResponse);
         }
         if (PaymentsResponse.ResultCodeEnum.RECEIVED == paymentsResponse.getResultCode()) {
+            return createOrderFromPaymentsResponse(paymentsResponse);
+        }
+        if (PaymentsResponse.ResultCodeEnum.PRESENTTOSHOPPER == paymentsResponse.getResultCode()) {
             return createOrderFromPaymentsResponse(paymentsResponse);
         }
         if (PaymentsResponse.ResultCodeEnum.REDIRECTSHOPPER == paymentsResponse.getResultCode()) {
