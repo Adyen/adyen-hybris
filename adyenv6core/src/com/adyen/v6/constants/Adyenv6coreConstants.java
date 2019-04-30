ea42afed65f589ab7853d69b2ac23b1a5c463e89
alexandros <alexandros.moraitis@adyen.com>
2019-04-30 11:46:41 +0200

    Update plugin version 4.2.0 boleto labels are capital

--- adyenv6core/src/com/adyen/v6/constants/Adyenv6coreConstants.java
+++ adyenv6core/src/com/adyen/v6/constants/Adyenv6coreConstants.java
@@ -31,7 +31,7 @@ public final class Adyenv6coreConstants extends GeneratedAdyenv6coreConstants {
 	public static final String EXTENSIONNAME = "adyenv6core";
 
 	public static final String PLUGIN_NAME = "adyen-hybris";
-	public static final String PLUGIN_VERSION = "4.1.0";
+	public static final String PLUGIN_VERSION = "4.2.0";
 	public static final String PAYMENT_PROVIDER = "Adyen";
 
 	final public static String PAYMENT_METHOD_CC = "adyen_cc";
