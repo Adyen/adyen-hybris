aaf7eec6e4b1db9e3523bb1f8e97278591f05b45
rajni <rajni.kewlani@adyen.com>
2019-06-27 16:28:23 +0200

    [Pw-1285]: Separated notification service from checkout addon

--- adyenv6notification/src/com/adyen/v6/security/AdyenNotificationAuthenticationProvider.java
+++ adyenv6notification/src/com/adyen/v6/security/AdyenNotificationAuthenticationProvider.java
@@ -20,38 +20,59 @@
  */
 package com.adyen.v6.security;
 
-import de.hybris.platform.store.BaseStoreModel;
-import de.hybris.platform.store.services.BaseStoreService;
-import org.apache.log4j.Logger;
-import org.springframework.util.Assert;
-
-import javax.servlet.http.HttpServletRequest;
 import java.nio.charset.Charset;
 import java.util.Base64;
+import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
+import javax.xml.ws.http.HTTPException;
+import org.apache.log4j.Logger;
+import org.springframework.stereotype.Component;
+import org.springframework.util.Assert;
+import de.hybris.platform.basecommerce.model.site.BaseSiteModel;
+import de.hybris.platform.site.BaseSiteService;
+import de.hybris.platform.store.BaseStoreModel;
+import de.hybris.platform.store.services.BaseStoreService;
 
 /**
  * Authenticates a request that is using Basic Authentication
  */
+@Component
 public class AdyenNotificationAuthenticationProvider {
+    @Resource(name = "baseStoreService")
     private BaseStoreService baseStoreService;
+
+    @Resource(name = "baseSiteService")
+    private BaseSiteService baseSiteService;
 
     private static final Logger LOG = Logger.getLogger(AdyenNotificationAuthenticationProvider.class);
 
-    public boolean authenticateBasic(final HttpServletRequest request) {
+    public boolean authenticateBasic(final HttpServletRequest request, String baseSiteId) {
         final String authorization = request.getHeader("Authorization");
         if (authorization != null && authorization.startsWith("Basic")) {
             String base64Credentials = authorization.substring("Basic".length()).trim();
-            String credentials = new String(Base64.getDecoder().decode(base64Credentials),
-                    Charset.forName("UTF-8"));
+            String credentials = new String(Base64.getDecoder().decode(base64Credentials), Charset.forName("UTF-8"));
             final String[] values = credentials.split(":", 2);
-
-            return tryToAuthenticate(values[0], values[1]);
+            return tryToAuthenticate(values[0], values[1], baseSiteId);
         }
         return false;
     }
 
-    private boolean tryToAuthenticate(String name, String password) {
+    private boolean tryToAuthenticate(String name, String password, String baseSiteId) {
+
+        LOG.debug("Trying to authenticate for baseSiteId " + baseSiteId);
+        final BaseSiteModel requestedBaseSite = getBaseSiteService().getBaseSiteForUID(baseSiteId);
+        if (requestedBaseSite != null) {
+            final BaseSiteModel currentBaseSite = getBaseSiteService().getCurrentBaseSite();
+
+            if (! requestedBaseSite.equals(currentBaseSite)) {
+                getBaseSiteService().setCurrentBaseSite(requestedBaseSite, true);
+            }
+        }
         BaseStoreModel baseStore = baseStoreService.getCurrentBaseStore();
+
+        if (baseStore == null) {
+            return false;
+        }
 
         String notificationUsername = baseStore.getAdyenNotificationUsername();
         String notificationPassword = baseStore.getAdyenNotificationPassword();
@@ -72,6 +93,10 @@ public class AdyenNotificationAuthenticationProvider {
 
     public BaseStoreService getBaseStoreService() {
         return baseStoreService;
+    }
+
+    protected BaseSiteService getBaseSiteService() {
+        return baseSiteService;
     }
 
     public void setBaseStoreService(BaseStoreService baseStoreService) {
