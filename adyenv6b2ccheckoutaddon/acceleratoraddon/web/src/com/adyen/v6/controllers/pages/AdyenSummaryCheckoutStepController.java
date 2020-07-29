51dc325c0fdd4160189c7dcbd22518e2582cda8d
renatoma <renato.martins@adyen.com>
2020-07-29 16:16:07 +0200

    Fix redirect payment flow for anonymous users

--- adyenv6b2ccheckoutaddon/acceleratoraddon/web/src/com/adyen/v6/controllers/pages/AdyenSummaryCheckoutStepController.java
+++ adyenv6b2ccheckoutaddon/acceleratoraddon/web/src/com/adyen/v6/controllers/pages/AdyenSummaryCheckoutStepController.java
@@ -290,7 +290,7 @@ public class AdyenSummaryCheckoutStepController extends AbstractCheckoutStepCont
         } catch (Exception e) {
             LOGGER.debug("Unexpected exception: " + e.getMessage(), e);
         }
-        LOGGER.debug("Redirecting to payment method with error: " + errorMessage);
+
         return redirectToSelectPaymentMethodWithError(redirectModel, errorMessage);
     }
 
@@ -316,7 +316,6 @@ public class AdyenSummaryCheckoutStepController extends AbstractCheckoutStepCont
             return REDIRECT_PREFIX + "/cart";
         }
 
-        LOGGER.debug("Redirecting to payment method with error: " + errorMessage);
         return redirectToSelectPaymentMethodWithError(redirectModel, errorMessage);
     }
 
@@ -341,7 +340,7 @@ public class AdyenSummaryCheckoutStepController extends AbstractCheckoutStepCont
                 case AUTHORISED:
                 case RECEIVED:
                     LOGGER.debug("Redirecting to order confirmation");
-                    OrderData orderData = orderFacade.getOrderDetailsForCode(response.getMerchantReference());
+                    OrderData orderData = orderFacade.getOrderDetailsForCodeWithoutUser(response.getMerchantReference());
                     if (orderData == null) {
                         throw new Exception("Order not found");
                     }
@@ -355,11 +354,8 @@ public class AdyenSummaryCheckoutStepController extends AbstractCheckoutStepCont
             }
         } catch (Exception e) {
             LOGGER.error(e);
+            return redirectToSelectPaymentMethodWithError(redirectModel, "checkout.error.authorization.payment.error");
         }
-
-        LOGGER.debug("Redirecting to cart..");
-        GlobalMessages.addFlashMessage(redirectModel, GlobalMessages.ERROR_MESSAGES_HOLDER, "checkout.error.authorization.payment.error");
-        return REDIRECT_PREFIX + "/cart";
     }
 
     /**
@@ -421,6 +417,7 @@ public class AdyenSummaryCheckoutStepController extends AbstractCheckoutStepCont
     }
 
     private String redirectToSelectPaymentMethodWithError(final RedirectAttributes redirectModel, final String messageKey) {
+        LOGGER.debug("Redirecting to payment method with error: " + messageKey);
         GlobalMessages.addFlashMessage(redirectModel, GlobalMessages.ERROR_MESSAGES_HOLDER, messageKey);
 
         return REDIRECT_PREFIX + AdyenControllerConstants.SELECT_PAYMENT_METHOD_PREFIX;
