d4f33b4b08c38fba8e4f5d50f9d41565996d2ed8
renatoma <renato.martins@adyen.com>
2020-07-31 16:45:32 +0200

    Small refactor on action classes / changes on order-process files

--- adyenv6core/src/com/adyen/v6/actions/order/AdyenCheckPendingOrderAction.java
+++ adyenv6core/src/com/adyen/v6/actions/order/AdyenCheckPendingOrderAction.java
@@ -24,39 +24,19 @@ package com.adyen.v6.actions.order;
 import de.hybris.platform.core.enums.OrderStatus;
 import de.hybris.platform.core.model.order.OrderModel;
 import de.hybris.platform.orderprocessing.model.OrderProcessModel;
-import de.hybris.platform.processengine.action.AbstractAction;
+import de.hybris.platform.processengine.action.AbstractSimpleDecisionAction;
 import org.apache.log4j.Logger;
 
-import java.util.HashSet;
-import java.util.Set;
-
-public class AdyenCheckPendingOrderAction extends AbstractAction<OrderProcessModel> {
+public class AdyenCheckPendingOrderAction extends AbstractSimpleDecisionAction<OrderProcessModel> {
 	private static final Logger LOG = Logger.getLogger(AdyenCheckPendingOrderAction.class);
 
-	public enum Transition {
-		OK, NOK;
-
-		public static Set<String> getStringValues() {
-			Set<String> res = new HashSet<>();
-			for (final Transition transitions : Transition.values()) {
-				res.add(transitions.toString());
-			}
-			return res;
-		}
-	}
-
 	@Override
-	public Set<String> getTransitions() {
-		return AdyenCheckPendingOrderAction.Transition.getStringValues();
-	}
-
-	@Override
-	public String execute(final OrderProcessModel process) {
+	public Transition executeAction(OrderProcessModel process) {
 		final OrderModel order = process.getOrder();
 
 		if (order == null) {
 			LOG.error("Missing the order, exiting the process");
-			return Transition.NOK.toString();
+			return Transition.NOK;
 		}
 
 		if (OrderStatus.PAYMENT_PENDING.equals(order.getStatus())) {
@@ -64,14 +44,14 @@ public class AdyenCheckPendingOrderAction extends AbstractAction<OrderProcessMod
 					+ " Order still on Payment Pending, will update status to PROCESSING_ERROR...");
 			order.setStatus(OrderStatus.PROCESSING_ERROR);
 			modelService.save(order);
-			return Transition.NOK.toString();
+			return Transition.NOK;
 
 		} else if (OrderStatus.CANCELLED.equals(order.getStatus())
 				|| OrderStatus.PROCESSING_ERROR.equals(order.getStatus())) {
 			LOG.debug("Process: " + process.getCode() + " Order Cancelled");
-			return Transition.NOK.toString();
+			return Transition.NOK;
 		}
 
-		return Transition.OK.toString();
+		return Transition.OK;
 	}
 }
