9ba1f13d3038c725f2e9f6a35041494bfa5199b0
Joseluis Alias <joseluis.alias@e2ycommerce.com>
2023-06-08 17:36:37 +0200

    Release 11.0.2 COMMIT

--- adyenv6core/src/com/adyen/v6/paymentmethoddetails/executors/impl/AdyenPaymentMethodDetailsStrategyExecutorImpl.java
+++ adyenv6core/src/com/adyen/v6/paymentmethoddetails/executors/impl/AdyenPaymentMethodDetailsStrategyExecutorImpl.java
@@ -2,6 +2,7 @@ package com.adyen.v6.paymentmethoddetails.executors.impl;
 
 import com.adyen.model.checkout.PaymentMethodDetails;
 import com.adyen.v6.paymentmethoddetails.builders.AdyenPaymentMethodDetailsBuilderStrategy;
+import com.adyen.v6.paymentmethoddetails.builders.impl.GenericAdyenPaymentMethodDetailsBuilderStrategy;
 import com.adyen.v6.paymentmethoddetails.executors.AdyenPaymentMethodDetailsBuilderExecutor;
 import de.hybris.platform.commercefacades.order.data.CartData;
 
@@ -9,10 +10,13 @@ import java.util.List;
 
 public class AdyenPaymentMethodDetailsStrategyExecutorImpl implements AdyenPaymentMethodDetailsBuilderExecutor {
 
-    protected final List<AdyenPaymentMethodDetailsBuilderStrategy<CartData>> strategies;
+    private final List<AdyenPaymentMethodDetailsBuilderStrategy<CartData>> strategies;
+    private final GenericAdyenPaymentMethodDetailsBuilderStrategy genericStrategy;
 
-    public AdyenPaymentMethodDetailsStrategyExecutorImpl(final List<AdyenPaymentMethodDetailsBuilderStrategy<CartData>> strategies) {
+    public AdyenPaymentMethodDetailsStrategyExecutorImpl(final List<AdyenPaymentMethodDetailsBuilderStrategy<CartData>> strategies,
+                                                         final GenericAdyenPaymentMethodDetailsBuilderStrategy genericStrategy) {
         this.strategies = strategies;
+        this.genericStrategy = genericStrategy;
     }
 
     @Override
@@ -21,6 +25,6 @@ public class AdyenPaymentMethodDetailsStrategyExecutorImpl implements AdyenPayme
                 .filter(strategy -> strategy.isApplicable(cartData))
                 .findAny()
                 .map(strategy -> strategy.buildPaymentMethodDetails(cartData))
-                .orElseThrow(() -> new RuntimeException("Not strategies were found for command request with payment type: [" + cartData.getAdyenPaymentMethod() + "]"));
+                .orElse(genericStrategy.buildPaymentMethodDetails(cartData));
     }
 }
