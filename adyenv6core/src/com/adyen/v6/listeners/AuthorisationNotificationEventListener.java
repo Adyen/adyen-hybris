ab8b31ab619bbd89dc8305533d0db4af18e992b0
kpieloch <113994423+kpieloch@users.noreply.github.com>
2023-10-02 17:03:07 +0200

    [AD-37] Authorisation notification event listener

---
+++ adyenv6core/src/com/adyen/v6/listeners/AuthorisationNotificationEventListener.java
@@ -0,0 +1,38 @@
+package com.adyen.v6.listeners;
+
+import com.adyen.v6.events.AuthorisationEvent;
+import com.adyen.v6.model.AdyenNotificationModel;
+import de.hybris.platform.payment.model.PaymentTransactionModel;
+import org.apache.log4j.Logger;
+
+import java.util.Date;
+
+public class AuthorisationNotificationEventListener extends AbstractNotificationEventListener<AuthorisationEvent> {
+
+
+    private static final Logger LOG = Logger.getLogger(AuthorisationNotificationEventListener.class);
+
+    public AuthorisationNotificationEventListener() {
+        super();
+    }
+
+    @Override
+    protected void onEvent(final AuthorisationEvent event) {
+        AdyenNotificationModel notificationInfoModel = event.getNotificationRequestItem();
+        PaymentTransactionModel transactionModel = getPaymentTransactionRepository().getTransactionModel(notificationInfoModel.getPspReference());
+        try {
+            if (transactionModel == null) {
+                getAdyenNotificationService().processAuthorisationEvent(notificationInfoModel);
+            } else {
+                LOG.warn("Authorisation already processed " + transactionModel.getRequestId());
+            }
+            LOG.info("Notification with PSPReference " + notificationInfoModel.getPspReference() + " was processed");
+            notificationInfoModel.setProcessedAt(new Date());
+            getModelService().save(notificationInfoModel);
+        }catch (Exception e) {
+            LOG.error("Notification with psp reference: " + notificationInfoModel.getPspReference() + " cause an exception. \n");
+            LOG.error("Exception: ", e);
+        }
+    }
+
+}
