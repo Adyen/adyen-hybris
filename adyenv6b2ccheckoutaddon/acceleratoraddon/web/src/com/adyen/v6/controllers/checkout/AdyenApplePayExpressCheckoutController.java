4bab1aa469e1c4d5f5bc9245d2e7f211914cd16a
PJaneta <p.janeta@reply.com>
2023-10-09 09:06:48 +0200

    AD-46 [BE][Express checkout] PDP controller

---
+++ adyenv6b2ccheckoutaddon/acceleratoraddon/web/src/com/adyen/v6/controllers/checkout/AdyenApplePayExpressCheckoutController.java
@@ -0,0 +1,22 @@
+package com.adyen.v6.controllers.checkout;
+
+import com.adyen.v6.request.ApplePayExpressPDPRequest;
+import org.apache.log4j.Logger;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+@Controller
+public class AdyenApplePayExpressCheckoutController {
+    private static final Logger LOG = Logger.getLogger(AdyenApplePayExpressCheckoutController.class);
+
+    @PostMapping("/expressCheckout/applePayPDP")
+    public ResponseEntity applePayExpressPDP(@RequestBody ApplePayExpressPDPRequest applePayExpressPDPRequest) {
+
+        LOG.info(applePayExpressPDPRequest);
+        LOG.info("applePayExpressPDPRequest");
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+}
