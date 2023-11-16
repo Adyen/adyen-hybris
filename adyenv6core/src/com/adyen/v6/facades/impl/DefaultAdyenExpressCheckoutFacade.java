ef4900f4bfa3e57418381c6d01c9fd01ef6791fc
PJaneta <p.janeta@reply.com>
2023-11-16 11:49:49 +0100

    AD-50 set user in session

--- adyenv6core/src/com/adyen/v6/facades/impl/DefaultAdyenExpressCheckoutFacade.java
+++ adyenv6core/src/com/adyen/v6/facades/impl/DefaultAdyenExpressCheckoutFacade.java
@@ -28,6 +28,7 @@ import de.hybris.platform.product.ProductService;
 import de.hybris.platform.servicelayer.dto.converter.Converter;
 import de.hybris.platform.servicelayer.i18n.CommonI18NService;
 import de.hybris.platform.servicelayer.model.ModelService;
+import de.hybris.platform.servicelayer.user.UserService;
 import org.apache.commons.collections.CollectionUtils;
 import org.apache.commons.lang3.StringUtils;
 import org.apache.commons.validator.routines.EmailValidator;
@@ -57,6 +58,7 @@ public class DefaultAdyenExpressCheckoutFacade implements AdyenExpressCheckoutFa
     private DeliveryModeService deliveryModeService;
     private ZoneDeliveryModeService zoneDeliveryModeService;
     private AdyenCheckoutFacade adyenCheckoutFacade;
+    private UserService userService;
     private Converter<AddressData, AddressModel> addressReverseConverter;
     private Converter<CartModel, CartData> cartConverter;
 
@@ -69,6 +71,8 @@ public class DefaultAdyenExpressCheckoutFacade implements AdyenExpressCheckoutFa
         }
 
         CustomerModel user = createGuestCustomer(addressData.getEmail());
+        userService.setCurrentUser(user);
+
         CartModel cart = createCartForExpressCheckout(user);
 
         AddressModel addressModel = addressReverseConverter.convert(addressData);
@@ -124,6 +128,7 @@ public class DefaultAdyenExpressCheckoutFacade implements AdyenExpressCheckoutFa
     public PaymentsResponse expressCartCheckout(AddressData addressData, String merchantId, String merchantName,
                                                 String applePayToken, HttpServletRequest request) throws Exception {
         CustomerModel user = createGuestCustomer(addressData.getEmail());
+        userService.setCurrentUser(user);
         cartService.changeCurrentCartUser(user);
 
         CartModel cart = cartService.getSessionCart();
@@ -275,5 +280,9 @@ public class DefaultAdyenExpressCheckoutFacade implements AdyenExpressCheckoutFa
 
     public void setCartConverter(Converter<CartModel, CartData> cartConverter) {
         this.cartConverter = cartConverter;
+    }
+
+    public void setUserService(UserService userService) {
+        this.userService = userService;
     }
 }
