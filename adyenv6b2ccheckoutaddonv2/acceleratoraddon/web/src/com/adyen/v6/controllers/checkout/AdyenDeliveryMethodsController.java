43d337cb088f7f4154c2664883974b632891c963
Lukasz756 <lgasiorowski0000@gmail.com>
2024-01-18 15:00:08 +0100

    AD-149&AD-148 Develop Endpoint for Saving Selected Delivery Method, Create API Endpoint for Retrieving Delivery Methods

--- adyenv6b2ccheckoutaddonv2/acceleratoraddon/web/src/com/adyen/v6/controllers/checkout/AdyenDeliveryMethodsController.java
+++ adyenv6b2ccheckoutaddonv2/acceleratoraddon/web/src/com/adyen/v6/controllers/checkout/AdyenDeliveryMethodsController.java
@@ -1,31 +1,47 @@
 package com.adyen.v6.controllers.checkout;
 
-import com.adyen.v6.facades.AdyenCheckoutFacade;
-import de.hybris.platform.acceleratorfacades.flow.CheckoutFlowFacade;
-import de.hybris.platform.commercefacades.user.data.AddressData;
+import com.adyen.v6.dtos.AdyenDeliveryMethodDTO;
+import de.hybris.platform.acceleratorstorefrontcommons.annotations.RequireHardLogIn;
+import de.hybris.platform.commercefacades.order.CheckoutFacade;
+import de.hybris.platform.commercefacades.order.data.DeliveryModeData;
+import org.apache.commons.lang.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+import java.util.List;
 
 
-@RestController
+@Controller
 @RequestMapping(value = "/checkoutApi/deliveryMethods")
 public class AdyenDeliveryMethodsController {
 
-    private static final String CARTID_PATH_VARIABLE_PATTERN = "{cartID:.*}";
-
     @Autowired
-    private AdyenCheckoutFacade adyenCheckoutFacade;
+    private CheckoutFacade checkoutFacade;
 
-    @GetMapping(value = "/{cartID}", produces = "application/json")
-    public ResponseEntity getAllDeliveryMethods(@PathVariable("cartID") final String cartID) {
-        return new ResponseEntity<>(adyenCheckoutFacade.getSupportedDeliveryModes(cartID), HttpStatus.OK);
+    @RequireHardLogIn
+    @GetMapping(value = "/getSupportedDeliveryMethods", produces = "application/json")
+    public ResponseEntity<List<? extends DeliveryModeData>> getAllDeliveryMethods() {
+        return new ResponseEntity<>(checkoutFacade.getSupportedDeliveryModes(), HttpStatus.OK);
+    }
+
+    @RequireHardLogIn
+    @PostMapping(value = "/selectDeliveryMethod", produces = "application/json")
+    public ResponseEntity<HttpStatus> selectDeliveryMethod(@RequestBody AdyenDeliveryMethodDTO deliveryMethod) {
+        if (StringUtils.isNotEmpty(deliveryMethod.getDeliveryMethodCode())) {
+            checkoutFacade.setDeliveryMode(deliveryMethod.getDeliveryMethodCode());
+            return ResponseEntity.status(HttpStatus.OK).build();
+        } else {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
+        }
+    }
+
+    public CheckoutFacade getCheckoutFacade() {
+        return checkoutFacade;
     }
 }
---
+++ adyenv6b2ccheckoutaddonv2/acceleratoraddon/web/src/com/adyen/v6/dtos/AdyenDeliveryMethodDTO.java
@@ -0,0 +1,13 @@
+package com.adyen.v6.dtos;
+
+public class AdyenDeliveryMethodDTO {
+    private String deliveryMethodCode;
+
+    public String getDeliveryMethodCode() {
+        return deliveryMethodCode;
+    }
+
+    public void setDeliveryMethodCode(String deliveryMethodCode) {
+        this.deliveryMethodCode = deliveryMethodCode;
+    }
+}
