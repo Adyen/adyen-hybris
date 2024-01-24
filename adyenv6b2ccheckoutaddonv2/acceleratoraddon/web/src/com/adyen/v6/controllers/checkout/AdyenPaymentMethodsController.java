90b4653c7c0390294a07e33b77c6902e25dd00f2
Lukasz756 <lgasiorowski0000@gmail.com>
2024-01-24 11:41:40 +0100

    AD-178 Controller for payment methods.

--- adyenv6b2ccheckoutaddonv2/acceleratoraddon/web/src/com/adyen/v6/controllers/checkout/AdyenPaymentMethodsController.java
+++ adyenv6b2ccheckoutaddonv2/acceleratoraddon/web/src/com/adyen/v6/controllers/checkout/AdyenPaymentMethodsController.java
@@ -1,24 +1,38 @@
 package com.adyen.v6.controllers.checkout;
 
 import com.adyen.v6.forms.AdyenPaymentForm;
+import com.adyen.v6.forms.validation.AdyenPaymentFormValidator;
 import de.hybris.platform.acceleratorstorefrontcommons.annotations.RequireHardLogIn;
 import de.hybris.platform.commercefacades.order.CheckoutFacade;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BeanPropertyBindingResult;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller(value = "/checkout/multi/adyen")
 public class AdyenPaymentMethodsController {
 
-  @Autowired
-  private CheckoutFacade checkoutFacade;
-  @RequireHardLogIn
-  @PostMapping(value = "/select-payment-method")
-  public ResponseEntity<HttpStatus> selectPaymentMethod(AdyenPaymentForm adyenPaymentForm){
-    adyenPaymentForm.getPaymentMethod();
-    checkoutFacade.payme
-    return
-  }
+    @Autowired
+    private CheckoutFacade checkoutFacade;
+
+    @Autowired
+    AdyenPaymentFormValidator adyenPaymentFormValidator;
+
+    @RequireHardLogIn
+    @PostMapping(value = "/select-payment-method")
+    public ResponseEntity<HttpStatus> selectPaymentMethod(AdyenPaymentForm adyenPaymentForm) {
+        final Errors errors = new BeanPropertyBindingResult(adyenPaymentForm, "payment");
+        adyenPaymentFormValidator.validate(adyenPaymentForm, errors);
+
+        if (errors.hasErrors()) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
+        }
+
+        checkoutFacade.setPaymentDetails(adyenPaymentForm.getPaymentMethod());
+
+        return ResponseEntity.status(HttpStatus.OK).build();
+    }
 }
