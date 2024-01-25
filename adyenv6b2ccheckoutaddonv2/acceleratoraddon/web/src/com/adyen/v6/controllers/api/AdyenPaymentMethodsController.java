029d9339ac1c2cc19b4d6afa6777374a6608d074
Lukasz756 <lgasiorowski0000@gmail.com>
2024-01-25 15:42:02 +0100

    AD-178 Delete unused methods and imports.

--- adyenv6b2ccheckoutaddonv2/acceleratoraddon/web/src/com/adyen/v6/controllers/api/AdyenPaymentMethodsController.java
+++ adyenv6b2ccheckoutaddonv2/acceleratoraddon/web/src/com/adyen/v6/controllers/api/AdyenPaymentMethodsController.java
@@ -2,10 +2,8 @@ package com.adyen.v6.controllers.api;
 
 import com.adyen.v6.facades.AdyenCheckoutFacade;
 import com.adyen.v6.forms.AdyenPaymentForm;
-import com.adyen.v6.forms.validation.AdyenPaymentFormValidator;
 import de.hybris.platform.acceleratorstorefrontcommons.annotations.RequireHardLogIn;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BeanPropertyBindingResult;
@@ -18,8 +16,6 @@ public class AdyenPaymentMethodsController {
     @Autowired
     private AdyenCheckoutFacade adyenCheckoutFacade;
 
-    @Autowired
-    AdyenPaymentFormValidator adyenPaymentFormValidator;
 
     @RequireHardLogIn
     @PostMapping(value = "/select-payment-method")
