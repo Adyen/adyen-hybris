ca4a2593026b9b0bca523caff43189ed4ea1b322
Lukasz756 <lgasiorowski0000@gmail.com>
2024-02-08 11:00:04 +0100

    AD-179 Code reformatted.

--- adyenv6b2ccheckoutaddonv2/acceleratoraddon/web/src/com/adyen/v6/controllers/api/AdyenTranslationsController.java
+++ adyenv6b2ccheckoutaddonv2/acceleratoraddon/web/src/com/adyen/v6/controllers/api/AdyenTranslationsController.java
@@ -23,17 +23,17 @@ public class AdyenTranslationsController {
 
     @Autowired
     private I18NService i18NService;
+
     @RequireHardLogIn
     @PostMapping(value = "/translations")
     public ResponseEntity<Map<String, String>> postWordsForTranslation(@RequestBody List<String> keys) {
 
         Map<String, String> translationMap = new HashMap<>();
+
         for (String key : keys) {
             String message = messageSource.getMessage(key, null, i18NService.getCurrentLocale());
-
-            translationMap.put(key,message);
+            translationMap.put(key, message);
         }
-
         return ResponseEntity.ok().body(translationMap);
     }
 }
