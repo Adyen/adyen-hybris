b7772e5d55a3e2c27fcdef37b7ff22794bd9c837
PJaneta <p.janeta@reply.com>
2024-02-29 09:59:56 +0100

    Shipping method selection fix

--- adyenv6b2ccheckoutaddonv2/acceleratoraddon/web/webroot/_ui/responsive/common/js/adyen-checkout/src/components/shipping-method/ShippingMethod.tsx
+++ adyenv6b2ccheckoutaddonv2/acceleratoraddon/web/webroot/_ui/responsive/common/js/adyen-checkout/src/components/shipping-method/ShippingMethod.tsx
@@ -6,7 +6,6 @@ import {connect} from "react-redux";
 import {AppState} from "../../reducers/rootReducer";
 import {CodeValueItem, ShippingMethodModel} from "../../reducers/types";
 import HTMLReactParser from "html-react-parser";
-import {StoreDispatch} from "../../store/store";
 import {routes} from "../../router/routes";
 import {Navigate} from "react-router-dom";
 import {ShippingAddressHeading} from "../common/ShippingAddressHeading";
@@ -18,11 +17,7 @@ interface StoreProps {
     shippingAddress: AddressData
 }
 
-interface DispatchProps {
-    setShippingMethod: (code: string) => void;
-}
-
-type Props = StoreProps & DispatchProps
+type Props = StoreProps
 
 interface State {
     redirectToNextStep: boolean
@@ -79,7 +74,7 @@ class ShippingMethod extends React.Component<Props, State> {
                             <InputDropdown testId={"delivery_method"}
                                            values={this.getDropdownItems()}
                                            onChange={(code) => {
-                                               this.props.setShippingMethod(code)
+                                               this.onShippingMethodChange(code)
                                            }}
                                            selectedValue={this.props.selectedShippingMethodCode}/>
                         </div>
@@ -101,8 +96,4 @@ const mapStateToProps = (state: AppState): StoreProps => ({
     shippingAddress: state.cartData.deliveryAddress
 })
 
-const mapDispatchToProps = (dispatch: StoreDispatch): DispatchProps => ({
-    setShippingMethod: (code: string) => dispatch({type: "shippingMethod/setShippingMethod", payload: code}),
-});
-
-export default connect(mapStateToProps, mapDispatchToProps)(ShippingMethod)
+export default connect(mapStateToProps)(ShippingMethod)
