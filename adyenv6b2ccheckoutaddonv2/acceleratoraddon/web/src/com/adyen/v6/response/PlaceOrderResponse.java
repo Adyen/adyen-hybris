adabf613715add8d9ec52bf280d35b9d26c7cd21
Lukasz756 <lgasiorowski0000@gmail.com>
2024-03-07 13:21:04 +0100

    AD-210 Removed imports.

--- adyenv6b2ccheckoutaddonv2/acceleratoraddon/web/src/com/adyen/v6/response/PlaceOrderResponse.java
+++ adyenv6b2ccheckoutaddonv2/acceleratoraddon/web/src/com/adyen/v6/response/PlaceOrderResponse.java
@@ -1,10 +1,5 @@
 package com.adyen.v6.response;
 
-import org.springframework.validation.ObjectError;
-
-import java.util.ArrayList;
-import java.util.List;
-
 public class PlaceOrderResponse {
     private String orderNumber;
     private String error;
