25ae64e792fe3572e4ec7841017c489b2940b086
Lukasz756 <lgasiorowski0000@gmail.com>
2024-03-12 13:49:55 +0100

    AD-212 Code reformat.

--- adyenv6core/src/com/adyen/v6/facades/AdyenOrderFacade.java
+++ adyenv6core/src/com/adyen/v6/facades/AdyenOrderFacade.java
@@ -4,5 +4,6 @@ import de.hybris.platform.commercefacades.order.data.OrderData;
 
 public interface AdyenOrderFacade {
     public String getPaymentStatus(final String orderCode, final Object sessionGuid);
+
     public OrderData getOrderDetailsForCode(final String code, final Object sessionGuid);
 }
