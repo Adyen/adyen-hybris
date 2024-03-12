548e98da64139bfe18c97720595e589202e4810f
Lukasz756 <lgasiorowski0000@gmail.com>
2024-03-12 13:41:41 +0100

    AD-212 AdyenPaymentStatusController, AdyenOrderFacade with its implementation.

--- adyenv6b2ccheckoutaddonv2/acceleratoraddon/web/src/com/adyen/v6/controllers/api/AdyenPaymentStatusController.java
+++ adyenv6b2ccheckoutaddonv2/acceleratoraddon/web/src/com/adyen/v6/controllers/api/AdyenPaymentStatusController.java
@@ -1,7 +1,15 @@
 package com.adyen.v6.controllers.api;
 
+import com.adyen.v6.facades.AdyenCheckoutFacade;
+import com.adyen.v6.facades.AdyenOrderFacade;
+import com.adyen.v6.repository.OrderRepository;
+import de.hybris.platform.acceleratorstorefrontcommons.annotations.RequireHardLogIn;
+import de.hybris.platform.acceleratorstorefrontcommons.constants.WebConstants;
+import de.hybris.platform.order.OrderService;
 import de.hybris.platform.payment.PaymentService;
-import de.hybris.platform.payment.model.PaymentTransactionEntryModel;
+import de.hybris.platform.servicelayer.exceptions.UnknownIdentifierException;
+import de.hybris.platform.servicelayer.session.SessionService;
+import de.hybris.platform.servicelayer.user.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
@@ -17,36 +25,36 @@ public class AdyenPaymentStatusController {
     @Autowired
     PaymentService paymentService;
 
-    @PostMapping("/get-payment-status")
-    public ResponseEntity<String> postIdForPaymentStatus(@RequestBody String transactionId) {
+    @Autowired
+    OrderService orderService;
 
-        PaymentTransactionEntryModel paymentTransactionEntry = paymentService.getPaymentTransactionEntry(transactionId);
-        if (paymentTransactionEntry != null) {
-            String transactionStatus = paymentTransactionEntry.getTransactionStatus();
-            String extracted = getMessageFromStatus(transactionStatus);
-            return ResponseEntity.ok().body(extracted);
-        } else {
+    @Autowired
+    OrderRepository orderRepository;
+
+    @Autowired
+    AdyenCheckoutFacade adyenCheckoutFacade;
+
+    @Autowired
+    UserService userService;
+
+    @Autowired
+    SessionService sessionService;
+
+    @Autowired
+    AdyenOrderFacade adyenOrderFacade;
+
+    @RequireHardLogIn
+    @PostMapping("/get-payment-status")
+    public ResponseEntity<String> postIdForPaymentStatus(@RequestBody String orderCode) {
+        Object attribute = sessionService.getAttribute(WebConstants.ANONYMOUS_CHECKOUT_GUID);
+
+        try {
+            String paymentStatus = adyenOrderFacade.getPaymentStatus(orderCode, attribute);
+            return ResponseEntity.ok().body(paymentStatus);
+        } catch (UnknownIdentifierException exception) {
             return ResponseEntity.badRequest().build();
         }
 
-
-    }
-
-    private String getMessageFromStatus(String transactionStatus) {
-        switch (transactionStatus) {
-            case "ACCEPTED": {
-                return "Completed.";
-            }
-            case "REJECTED": {
-                return "Rejected.";
-            }
-            case "REVIEW": {
-                return "Waiting";
-            }
-            default: {
-                return "Error.";
-            }
-        }
     }
 
 }
---
+++ adyenv6core/src/com/adyen/v6/facades/AdyenOrderFacade.java
@@ -0,0 +1,8 @@
+package com.adyen.v6.facades;
+
+import de.hybris.platform.commercefacades.order.data.OrderData;
+
+public interface AdyenOrderFacade {
+    public String getPaymentStatus(final String orderCode, final Object sessionGuid);
+    public OrderData getOrderDetailsForCode(final String code, final Object sessionGuid);
+}
