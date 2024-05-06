531454430aedeacee6e21cbb9a57eb1434beacda
Lukasz756 <lgasiorowski0000@gmail.com>
2024-05-06 13:48:35 +0200

    AD-248 Controller with configuration retrieving method.

--- adyenocc/src/com/adyen/commerce/controllers/AdyenoccConfigurationController.java
+++ adyenocc/src/com/adyen/commerce/controllers/AdyenoccConfigurationController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 
 @RestController
-@RequestMapping(value = "/{baseSiteId}/")
+@RequestMapping(value = "/{baseSiteId}")
 public class AdyenoccConfigurationController {
 
     @Autowired
