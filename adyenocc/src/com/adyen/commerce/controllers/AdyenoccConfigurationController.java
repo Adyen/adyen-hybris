391688a6790873c23bd6a3ba2ae8ce4f1f088286
Lukasz756 <lgasiorowski0000@gmail.com>
2024-05-06 13:47:53 +0200

    AD-248 Controller with configuration retrieving method.

---
+++ adyenocc/src/com/adyen/commerce/controllers/AdyenoccConfigurationController.java
@@ -0,0 +1,27 @@
+package com.adyen.commerce.controllers;
+
+import com.adyen.service.exception.ApiException;
+import com.adyen.v6.dto.CheckoutConfigDTO;
+import com.adyen.v6.facades.AdyenCheckoutFacade;
+import de.hybris.platform.acceleratorstorefrontcommons.annotations.RequireHardLogIn;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+@RequestMapping(value = "/{baseSiteId}/")
+public class AdyenoccConfigurationController {
+
+    @Autowired
+    private AdyenCheckoutFacade adyenCheckoutFacade;
+
+    @RequireHardLogIn
+    @GetMapping(value = "/checkout-configuration")
+    public ResponseEntity<CheckoutConfigDTO> getCheckoutConfig() throws ApiException {
+        return ResponseEntity.ok().body(adyenCheckoutFacade.getReactCheckoutConfig());
+    }
+
+}
