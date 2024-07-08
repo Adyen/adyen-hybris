dda8ccf68bc241839c3312bba44bd3cdf114a019
PJaneta <p.janeta@reply.com>
2024-07-08 09:50:22 +0200

    AD-282 Improve Error Handling and User Guidance for Payment Transactions

--- adyenocc/src/com/adyen/commerce/exceptionhandler/AdyenOCCControllerExceptionHandler.java
+++ adyenocc/src/com/adyen/commerce/exceptionhandler/AdyenOCCControllerExceptionHandler.java
@@ -1,6 +1,7 @@
 package com.adyen.commerce.exceptionhandler;
 
 import com.adyen.commerce.exception.AdyenControllerException;
+import com.adyen.commerce.response.ErrorResponse;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -9,7 +10,7 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 public class AdyenOCCControllerExceptionHandler {
 
     @ExceptionHandler(value = AdyenControllerException.class)
-    public ResponseEntity<Void> handleAdyenControllerException(AdyenControllerException exception) {
-        return ResponseEntity.badRequest().build();
+    public ResponseEntity<ErrorResponse> handleAdyenControllerException(AdyenControllerException exception) {
+        return ResponseEntity.badRequest().body(exception.getErrorResponse());
     }
 }
