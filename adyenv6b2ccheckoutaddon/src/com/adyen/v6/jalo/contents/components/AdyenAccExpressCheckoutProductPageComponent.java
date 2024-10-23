cc9c862013621addbe381d7e77b523031a75b863
PJaneta <p.janeta@reply.com>
2024-10-23 14:50:11 +0200

    AD-335 Add Google Pay Express Button with Frontend Logic and API Calls to CMS Components on PDP and Cart Pages - add jalo classes

---
+++ adyenv6b2ccheckoutaddon/src/com/adyen/v6/jalo/contents/components/AdyenAccExpressCheckoutProductPageComponent.java
@@ -0,0 +1,25 @@
+package com.adyen.v6.jalo.contents.components;
+
+import de.hybris.platform.jalo.Item;
+import de.hybris.platform.jalo.JaloBusinessException;
+import de.hybris.platform.jalo.SessionContext;
+import de.hybris.platform.jalo.type.ComposedType;
+import org.apache.log4j.Logger;
+
+public class AdyenAccExpressCheckoutProductPageComponent extends GeneratedAdyenAccExpressCheckoutProductPageComponent
+{
+	@SuppressWarnings("unused")
+	private static final Logger LOG = Logger.getLogger( AdyenAccExpressCheckoutProductPageComponent.class.getName() );
+	
+	@Override
+	protected Item createItem(final SessionContext ctx, final ComposedType type, final ItemAttributeMap allAttributes) throws JaloBusinessException
+	{
+		// business code placed here will be executed before the item is created
+		// then create the item
+		final Item item = super.createItem( ctx, type, allAttributes );
+		// business code placed here will be executed after the item was created
+		// and return the item
+		return item;
+	}
+	
+}
